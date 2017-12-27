b8ed18418cbb4a2b9e49c860d1e9bc0ca7c61e47
Jyo <jf2920@nyu.edu>
2017-12-27 00:09:04 +0000

    first demo

--- scripts.js
+++ scripts.js
@@ -1,16 +1,28 @@
-$(function() {
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > 50) {
-          $(‘body’).addClass(‘colorChange’)
-          $(‘header’).addClass(‘displayNone’)
-          $(‘nav’).removeClass(‘navBackgroundStart’)
-          $(‘nav ul’).addClass(‘addBlackBackground’)
-       } 
-       if ($(this).scrollTop() < 50) {
-          $(‘body’).removeClass(‘colorChange’)
-          $(‘header’).removeClass(‘displayNone’)
-          $(‘nav’).addClass(‘navBackgroundStart’)
-          $(‘nav ul’).removeClass(‘addBlackBackground’)
-       } 
-    });
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("overlay-nav").style.display = "inline"; 
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("overlay-nav").style.display = "none"; 
+}
+
+
+$(document).ready(function(){
+    $("p").click(function(){
+        $(this).hide();
     })
+    
+    $(".links").eq(0).on("click", function(){
+        $("section").empty();
+        $(".overlay").css('display', 'none');
+        
+        $("section").load("about.html");
+    })
+    //menu
+    $(".links").eq(1).on("click", function(){
+        $("section").empty();
+         $("section").load("projects.html");
+       })
+});
