e700cafd9198e8dcedb4f2206d12cb5a76022032
Jyo <jf2920@nyu.edu>
2017-12-27 00:39:03 +0000

    edits

--- scripts.js
+++ scripts.js
@@ -1,27 +1,31 @@
 function openNav() {
+    document.getElementById("section").style.display = "none";
     document.getElementById("myNav").style.height = "100%";
     document.getElementById("overlay-nav").style.display = "inline"; 
+    document.getElementById("nav").style.display = "none";
 }
 
 function closeNav() {
-    document.getElementById("myNav").style.height = "0%";
     document.getElementById("overlay-nav").style.display = "none"; 
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("nav").style.display = "inline";
+    document.getElementById("section").style.display = "inline";
 }
 
 
 $(document).ready(function(){
+
     $("p").click(function(){
         $(this).hide();
     })
     
-    $(".links").eq(0).on("click", function(){
+    $(".links").eq(1).on("click", function(){
         $("section").empty();
         $(".overlay").css('display', 'none');
-        
         $("section").load("about.html");
     })
     //menu
-    $(".links").eq(1).on("click", function(){
+    $(".links").eq(2).on("click", function(){
         $("section").empty();
          $("section").load("projects.html");
        })
