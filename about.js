13c391f3ed04c2e80bb6a2ee97958394bd23ddbe
Jyo <jf2920@nyu.edu>
2018-01-04 21:01:16 +0000

    edits to about page

---
+++ about.js
@@ -0,0 +1,10 @@
+
+
+$(document).ready(function(){
+    // Animation complete
+    // $("#britain").fadeIn(10000);
+     // $("#arrow1").fadeIn("slow");z
+     // $("#nyu").fadeIn(3000);
+
+      
+  });
--- scripts.js
+++ scripts.js
@@ -14,7 +14,6 @@ function closeNav() {
 
 
 $(document).ready(function(){
-    
 
     $("p").click(function(){
         $(this).hide();
@@ -23,113 +22,59 @@ $(document).ready(function(){
         $("section").empty();
         $(".overlay").css('display', 'none');
         $("#nav").css('display', 'inline');
-    })
-/*
-    $(".links").eq(0).click(function(){
-        $("section").empty();
-        alert( "Load was performed." );
-        $(".overlay").css('display', 'none');
-        $("#nav").css('display', 'inline');
-        $("#about-page").css('display', 'inline');
-    })
-    
-    $(".links").eq(1).click(function(){
-        $("section").empty();
-        $(".overlay").css('display', 'none');
-        $("section").load("about.html");
-        $("#nav").css('display', 'inline');
-       
-        $("section").load("about.html");
+    }); 
 
-
-/*
-       var wHeight = $(window).innerHeight();
-       var siblings = $('.slide').siblings();
-       var perset = {};
-       var sumHeight = 0;
-       for(var i = 0; i<siblings.length; i++) {
-         if(siblings[i].dataset.background){
-           perset[sumHeight] =  siblings[i].dataset.background;
-         }
-         else
-           perset[sumHeight] =  0;
-     
-         sumHeight= sumHeight + siblings[i].clientHeight;
-       }
-       processScroll();
-     
-       function lessThan(nums, key){
-         if(nums == null || nums.length == 0 || key ==0 ) 
-           return 0;
-         low = 0;
-         high = nums.length -1;
-         while(low <= high){
-             mid = parseInt((low + high) >> 1);
-             if(key <= nums[mid]){
-                 high = mid - 1;
-             }else {
-                 low = mid +1;
-             }
-         }
-         return high;
-       }
-     
-       var scroll_pos = 0;
-     
-       function processScroll() { 
-         scroll_pos = $(this).scrollTop();
-     
-         var presetHeights = Object.keys(perset);
-         var x = lessThan(presetHeights,scroll_pos);
-         var bgColor = perset[presetHeights[x]];
-         if(bgColor) {
-           $("body").css({'background-color': bgColor,
-                        'transition' : '0.3s all'});
-
-         }
-       }
-     
-       $(document).scroll(processScroll); */
-
-       $(window).scroll(function() {
+    $(window).scroll(function() {
 
         // calculate the percentage the user has scrolled down the page
         var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
         if(scrollPercent > 0){
             $("body").addClass("changeColor")
-       }
-       if (scrollPercent < 10) {
-          $("body").removeClass("changeColor")
-       }
+        }
+        if (scrollPercent < 10) {
+            $("body").removeClass("changeColor")
+        }
+        if (scrollPercent < 20) {
+            $("#pencil").addClass("animated slideLeft"); 
+        }
+    });
 
-        });
-
-        $(window).scroll(function() {
-            $('#cake-img').each(function(){
-            var imagePos = $(this).offset().top;
-    
+    $(window).scroll(function() {
+        $('#cake-img').each(function(){
+        var imagePos = $(this).offset().top;
+        
             var topOfWindow = $(window).scrollTop();
-                if (imagePos < topOfWindow + 40) {
-                    $(this).addClass("slideUp");
-                }
-            });
+            if (imagePos < topOfWindow + 40) {
+                $(this).addClass("animated fadeIn");
+            }
         });
+    });
+
+    //about page
+    //$('#britain').addClass('animation').css('display', 'block');
+    /*
+    $( '#britain' ).animate({
+        left: '+=50', // increase by 50
+        opacity: 0.25,
+        fontSize: '12px'
+      },
+      300,
+    function(){
+        alert('he'); 
+    }); 
+    $('#arrow1').addClass('animation').css('display', 'block');*/
+    //$('#britain').css('visibility', 'visible'); 
+
+  // $("#britain").animate({opacity: '1'}, "slow");
 
 });
 
-/*
-$(function() {
-    const div1 = $(".wrapper");
-    $(window).scroll(function () {
-       if ($(this).scrollTop() > div1.height()) {
-          $("body").addClass("changeColor")
-       }
-       if ($(this).scrollTop() < div1.height()) {
-          $("body").removeClass("changeColor")
-       }
-    });
- });*/
+
+
+// If using jQuery 1.3 or lower, you need to do $(paras[i++] || []) to avoid an "undefined" error
+
+
+
 
 
  
-
