1104fe54f83a6c2bf0ba2d6a9d360e1843f1b766
Jyo <jf2920@nyu.edu>
2018-01-11 19:07:06 +0000

    about added to main scroll

--- scripts.js
+++ scripts.js
@@ -1,48 +1,54 @@
 function openNav() {
     document.getElementById("section").style.display = "none";
     document.getElementById("myNav").style.height = "100%";
-    document.getElementById("overlay-nav").style.display = "inline"; 
+    document.getElementById("overlay-nav").style.display = "inline";
     document.getElementById("nav").style.display = "none";
 }
 
 function closeNav() {
-    document.getElementById("overlay-nav").style.display = "none"; 
+    document.getElementById("overlay-nav").style.display = "none";
     document.getElementById("myNav").style.height = "0%";
     document.getElementById("nav").style.display = "inline";
     document.getElementById("section").style.display = "inline";
 }
 
 
-$(document).ready(function(){
+$(document).ready(function() {
 
-    $("p").click(function(){
+    $("p").click(function() {
         $(this).hide();
     })
-    $("#home-btn").click(function(){
+    $("#home-btn").click(function() {
         $("section").empty();
         $(".overlay").css('display', 'none');
         $("#nav").css('display', 'inline');
-    }); 
+    });
 
     $(window).scroll(function() {
 
         // calculate the percentage the user has scrolled down the page
         var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height());
-        if(scrollPercent > 0){
+        console.log(scrollPercent);
+        if (scrollPercent > 0) {
             $("body").addClass("changeColor")
         }
         if (scrollPercent < 10) {
             $("body").removeClass("changeColor")
         }
-        if (scrollPercent < 20) {
-            $("#pencil").addClass("animated slideLeft"); 
+        if (scrollPercent > 20) {
+            $("#pencil").addClass("animated slideLeft");
+            $("#headshot-homepage").addClass("animated fadeIn");
+            console.log("reached 20");
+        }
+        if (scrollPercent > 75) {
+            $("body").removeClass("changeColor")
         }
     });
 
     $(window).scroll(function() {
-        $('#cake-img').each(function(){
-        var imagePos = $(this).offset().top;
-        
+        $('#cake-img').each(function() {
+            var imagePos = $(this).offset().top;
+
             var topOfWindow = $(window).scrollTop();
             if (imagePos < topOfWindow + 40) {
                 $(this).addClass("animated fadeIn");
@@ -51,7 +57,3 @@ $(document).ready(function(){
     });
 
 });
-
-
-
- 
